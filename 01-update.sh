6c532660c5ac6bbf9696b808b19e6fec9111a0e8
adilsonpsantos3 <57467825+adilsonpsantos3@users.noreply.github.com>
2020-11-17 20:18:08 -0300

    Add files via upload

---
+++ 01-update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "atualizando as listas do app"              
+    apt update
+
+echo -e "atualizando os softwares"
+     apt -y upgrade
+
+echo -e "atualização do sistema"
+      apt -y full upgrade 
+
+echo -e "forçando atualização de segurança"
+      apt -y dist-upgrade
+
+echo -e  "removendo software desnecessários" 
+      apt -y autoremove      
+      apt -y autoclean
+      apt -y clean
+      
