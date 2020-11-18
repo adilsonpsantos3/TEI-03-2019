56f079c1fc5a44339cbd8709803d049c4261e56f
adilsonpsantos3 <57467825+adilsonpsantos3@users.noreply.github.com>
2020-11-17 21:32:25 -0300

    Add files via upload

---
+++ 02-lamp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+
+echo -e "Atualizando as listas do Apt"
+
+apt update
+
+echo -e "Instalando o LAMP Server no Ubuntu Server"
+
+#obs: utilizar a expressão regular ^ que casa com todas as opções
+#referente ao LAMP (Linux,Apache2,MySQL,PHP7.x)
+
+apt -y install lamp-server~perl python
